f28661d5b283ba39ddc22d14f23d822b380959e2
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-05-08 16:25:53 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+const chefs = require('./Data/Chef.json') 
+app.use(cors());
+app.get('/', (req, res)=>{
+    res.send('the menu is running')
+})
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
+})
+app.listen(port, ()=>{
+    console.log(`Menu API is running on port ${port}`);
+})
